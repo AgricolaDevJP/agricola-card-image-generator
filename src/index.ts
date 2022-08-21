818c48aef836204ce0d6639e91ced5bb79963796
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2022-08-21 16:25:36 +0900

    use queryparameters for generating img

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,20 @@ import type {
   Context,
   APIGatewayProxyResult,
   APIGatewayEvent,
+  APIGatewayProxyEventQueryStringParameters,
 } from "aws-lambda";
 import chromium from "@sparticuz/chrome-aws-lambda";
 import puppeteer from "puppeteer-core";
+
+type GenerateCardParams = GenerateOccupationParams;
+
+interface GenerateOccupationParams {
+  id: string | undefined;
+  name: string;
+  description: string;
+  cardType: "occupation";
+  minPlayers: 1 | 3 | 4 | 5 | 6;
+}
 
 let template: hogan.Template | undefined;
 let browser: puppeteer.Browser | undefined;
@@ -15,8 +26,10 @@ export const handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  const [image, error] = await generateCardImage();
-  if (error) {
+  let params: GenerateCardParams | undefined;
+  try {
+    params = validateParameters(event.queryStringParameters);
+  } catch (error) {
     console.error(error);
     return {
       statusCode: 400,
@@ -24,6 +37,19 @@ export const handler = async (
       body: JSON.stringify({ error }),
     };
   }
+
+  let image: string | undefined;
+  try {
+    image = await generateCardImage(params);
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ error }),
+    };
+  }
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "image/png" },
@@ -32,7 +58,40 @@ export const handler = async (
   };
 };
 
-const generateCardImage = async (): Promise<[string, unknown | undefined]> => {
+const validateParameters = (
+  queryStringParams: APIGatewayProxyEventQueryStringParameters | null
+): GenerateCardParams => {
+  if (queryStringParams === null) {
+    throw Error("parameters are undefined");
+  }
+  switch (queryStringParams.cardType) {
+    case "occupation":
+      const minPlayers = Number(queryStringParams.minPlayers!);
+      if (
+        minPlayers !== 1 &&
+        minPlayers !== 3 &&
+        minPlayers !== 4 &&
+        minPlayers !== 5 &&
+        minPlayers !== 6
+      ) {
+        throw Error(`minPlayers must be 1, 3, 4, 5 or 6: ${minPlayers}`);
+      }
+
+      return {
+        id: queryStringParams.id || undefined,
+        name: queryStringParams.name!,
+        description: queryStringParams.description!,
+        cardType: "occupation",
+        minPlayers,
+      };
+    default:
+      throw Error(`unexpected cardtype: ${queryStringParams.cardType}`);
+  }
+};
+
+const generateCardImage = async (
+  params: GenerateCardParams
+): Promise<string> => {
   if (template === undefined) {
     const templateHtml = await fs.readFile("./assets/index.html", {
       encoding: "utf-8",
@@ -41,10 +100,10 @@ const generateCardImage = async (): Promise<[string, unknown | undefined]> => {
   }
 
   const html = template.render({
-    name: "欠陥建築設計士",
-    id: "AR018",
-    minPlayers: 1,
-    description: `1部屋建設するたびにその建設コストを好きな資材2つ少なくできる。そうした場合、建設した部屋には人物を収容できない。（部屋タイルを斜めに配置して区別する。）`,
+    name: params.name,
+    id: params.id || "",
+    minPlayers: params.minPlayers,
+    description: params.description,
   });
 
   let page: puppeteer.Page | undefined;
@@ -71,9 +130,9 @@ const generateCardImage = async (): Promise<[string, unknown | undefined]> => {
       type: "png",
       encoding: "base64",
     })) as string;
-    return [screenshot, undefined];
+    return screenshot;
   } catch (error: unknown) {
-    return ["", error];
+    throw error;
   } finally {
     if (page !== undefined) {
       await page.close();
